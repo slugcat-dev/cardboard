67b6da39ba913e623320336dbf9476e44e188d06
slugcat-dev <57626963+slugcat-dev@users.noreply.github.com>
2023-12-26 14:57:33 +0100

    Test

--- server/api/test.get.ts
+++ server/api/test.get.ts
@@ -1,11 +1,16 @@
-import {
-	getRequestProtocol,
-	getRequestURL
-} from 'h3'
+import { getRequestURL } from 'h3'
+import type { H3Event } from 'h3'
 
 export default defineEventHandler(async (event) => {
 	return [
-		getRequestProtocol(event),
-		getRequestURL(event)
+		getRequestProtocol(event, { xForwardedProto: true }),
+		getRequestURL(event, { xForwardedProto: true })
 	]
 })
+
+export function getRequestProtocol(event: H3Event, opts: { xForwardedProto?: boolean } = {}) {
+	if (opts.xForwardedProto !== false && event.node.req.headers['x-forwarded-proto'] === 'https')
+		return 'https'
+
+	return (event.node.req.connection as any)?.encrypted ? 'https' : 'http'
+}
