5e403992dabc39a0349144b74673da545ffb97f8
slugcat-dev <57626963+slugcat-dev@users.noreply.github.com>
2024-07-21 06:16:49 +0200

    Update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,49 +1,58 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
-	modules: [
-		'@vueuse/nuxt',
-		'nuxt-auth-utils',
-		'nuxt-icon',
-		'nuxt-mongoose'
+ devtools: { enabled: true },
+
+ modules: [
+					'@vueuse/nuxt',
+					'nuxt-auth-utils',
+					'nuxt-icon',
+					'nuxt-mongoose'
 	],
-	runtimeConfig: {
-		google: {
-			clientId: '',
-			clientSecret: ''
-		},
-		github: {
-			clientId: '',
-			clientSecret: ''
-		},
-		session: {
-			name: 'session',
-			password: '',
-			maxAge: 86400 * 30
-		}
+
+ runtimeConfig: {
+					google: {
+									clientId: '',
+									clientSecret: ''
+					},
+					github: {
+									clientId: '',
+									clientSecret: ''
+					},
+					session: {
+									name: 'session',
+									password: '',
+									maxAge: 86400 * 30
+					}
 	},
-	app: {
-		pageTransition: { name: 'slide' },
-		layoutTransition: { name: 'slide' },
-		rootId: 'app'
+
+ app: {
+					pageTransition: { name: 'slide' },
+					layoutTransition: { name: 'slide' },
+					rootId: 'app'
 	},
-	css: [
-		'~/assets/style.scss',
-		'~/assets/transitions.scss'
+
+ css: [
+					'~/assets/style.scss',
+					'~/assets/transitions.scss'
 	],
-	experimental: {
-		asyncContext: true
+
+ experimental: {
+					asyncContext: true
 	},
-	vite: {
-		build: {
-			target: 'esnext'
-		}
+
+ vite: {
+					build: {
+									target: 'esnext'
+					}
 	},
-	nitro: {
-		esbuild: {
-			options: {
-				target: 'esnext'
-			}
-		}
-	}
+
+ nitro: {
+					esbuild: {
+									options: {
+													target: 'esnext'
+									}
+					}
+	},
+
+ compatibilityDate: '2024-07-21'
 })
